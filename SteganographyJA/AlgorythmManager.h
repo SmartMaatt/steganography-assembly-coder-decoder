b01078283078a055b1bb2efea3205257489b0168
mateuszplonka.tg@gmail.com <mateuszplonka.tg@gmail.com>
2020-10-23 18:39:33 +0200

    6.2 Sprawdzanie CPUID oraz MMX *Procedura sprawdza procesor użytkownika pod względem obsługi CPUID oraz MMX *Wywołanie funkcji sprawdzającej po wciśnięciu Encode/Decode i wyświetlenie stosownego komunikatu w przypadku błędu

--- SteganographyJA/AlgorythmManager.h
+++ SteganographyJA/AlgorythmManager.h
@@ -5,6 +5,7 @@
 #include "FileManager.h"
 #include "cppSteganography.h"
 
+extern "C" bool checkMMXCapability();
 extern "C" void asmSteganographyEncode(unsigned char* bmpKey, char* symbol);
 extern "C" void asmSteganographyDecode(unsigned char* bmpKey, char* symbol);
 
--- SteganographyJA/asmSteganography.asm
+++ SteganographyJA/asmSteganography.asm
@@ -1,5 +1,33 @@
 .CODE
 
+checkMMXCapability PROC C
+
+	push rbx			; Zapisz RBX
+	pushfq				; Wypchnij flagi
+	pop rax				; Pobierz je do RAX
+	mov rbx, rax		; Zapisz do RBX na pozniej
+	xor rax, 200000h	; Przelacz id flagi
+	push rax			; Wypchnij przelaczone flagi
+	popfq				; Pobierz je z powrotem do flag, zresetowały sie?
+	pushfq				; Wypchnij flagi
+	pop rax				; Pobierz je z powrotem do RAX
+	cmp rax, rbx		; Porownaj aktualne flagi z wczesniej zapisanymi
+	jz NoToSzkoda		; Jesli się zresetowały, nie mozesz uzywać CPUID
+
+	pop rbx				; Przywracam RBX
+	mov eax, 1			; Nie zresetowało sie wiec...
+	cpuid				; CPUID_0000_0001
+	shr rdx, 23			; Shiftuj bit MMX do pozycji najbardziej po prawej
+	and rdx, 1			; Wykonuje AND na rdx (jesli bylo 1, pozostanie 1)
+	mov rax, rdx		; Przenosze RDX do akumulatora
+	ret					; Zwracam 0/1
+
+NoToSzkoda:
+	pop rbx				; Procesor nie obsluguje CPUID lub MMX
+	xor rax, rax		; Resetuj akumulator do 0
+	ret
+CheckMMXCapability ENDP
+
 
 asmSteganographyEncode PROC C ;bmpKey, symbol
 	
@@ -159,12 +187,6 @@ endProc:
 
 asmSteganographyEncode ENDP
 
-
-;---------------------------------------------------------------------------------------
-;---------------------------------------------------------------------------------------
-;									DEKODOWANIE TEKSTU
-;---------------------------------------------------------------------------------------
-;---------------------------------------------------------------------------------------
 
 asmSteganographyDecode PROC C ;bmpKey, symbol
 
